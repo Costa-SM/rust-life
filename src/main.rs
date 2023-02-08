806bff9e174001a08ebe3837a4807b22d6c64409
Costa-SM <arthurcstevenson@gmail.com>
2023-02-08 12:05:55 -0300

    Final fixes for base implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::default;
+
 // Macroquad for graphics
 use macroquad::prelude::*;
 use world::WorldState;
@@ -7,11 +9,40 @@ mod cell;
 mod world;
 mod camera;
 
-// Auxiliary things
-const ROW_LENGTH: f32 = 64f32; // Number of cells in a row. World will be composed of ROW_LENGTH^2 cells.
+// Auxiliary things.
+const ROW_LENGTH: f32 = 128f32; // Number of cells in a row. World will be composed of ROW_LENGTH^2 cells.
+
+fn write_text(x: f32, y: f32, text: &str, font: Font) {
+    let text_dims = measure_text(text, Some(font), 30u16, 1.0f32);
+    
+    draw_rectangle(
+        x - text_dims.width * 0.5f32,
+        y - text_dims.height * 0.5f32,
+        text_dims.width + 10f32,
+        text_dims.height + 10f32,
+        BLACK);
+
+    draw_text_ex(
+        &text, 
+        x - text_dims.width * 0.5f32 + 5f32,
+        y + text_dims.height * 0.5f32, 
+        TextParams{font, font_size: 30u16, color: WHITE, ..Default::default()}
+    );
+}
+// Window configuration
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Game of Life".to_string(),
+        window_resizable: false,
+        window_height: 900,
+        window_width: 900,
+        ..Default::default()
+    }
+}
+
 
 // Main Function
-#[macroquad::main("Game of Life")]
+#[macroquad::main(window_conf)]
 async fn main() {
     let mut world = world::CellWorld::new(ROW_LENGTH);
     let font = load_ttf_font("res/Heebo-VariableFont_wght.ttf").await.unwrap();
@@ -20,6 +51,7 @@ async fn main() {
         // Draw world before everything else, so that we can draw on top of it, if necessary.
         world.draw_world(font);
 
+        // Handle input
         if is_key_pressed(KeyCode::Space) {
             world.switch_state();
         }
@@ -35,47 +67,18 @@ async fn main() {
         if is_mouse_button_released(MouseButton::Left) {
             let mp = mouse_position();
             world.change_status(mp.0, mp.1);
-            }
-
+        }
+            
+        // Update the world.
         world.update_world();
-
         match world.get_state() {
             WorldState::Running => {
                 world.run_world();
             }
 
             WorldState::Stopped => {
-                let pause_text = "Press Space to Resume";
-                let pause_dims = measure_text(&pause_text, Some(font), 30u16, 1.0f32);
-                
-                draw_rectangle(screen_width() * 0.5f32 - pause_dims.width * 0.5f32 - 5f32,
-                    50f32 - pause_dims.height - 5f32,
-                    pause_dims.width + 10f32,
-                    pause_dims.height + 10f32,
-                    BLACK);
-
-                draw_text_ex(
-                    &pause_text, 
-                    screen_width() * 0.5f32 - pause_dims.width * 0.5f32, 
-                    45f32, 
-                    TextParams{font, font_size: 30u16, color: WHITE, ..Default::default()}
-                );
-
-                let pause_text = "Press R to Reset";
-                let pause_dims = measure_text(&pause_text, Some(font), 30u16, 1.0f32);
-                
-                draw_rectangle(screen_width() * 0.5f32 - pause_dims.width * 0.5f32 - 5f32,
-                    80f32 - pause_dims.height - 5f32,
-                    pause_dims.width + 10f32,
-                    pause_dims.height + 10f32,
-                    BLACK);
-
-                draw_text_ex(
-                    &pause_text, 
-                    screen_width() * 0.5f32 - pause_dims.width * 0.5f32, 
-                    80f32, 
-                    TextParams{font, font_size: 30u16, color: WHITE, ..Default::default()}
-                );
+                write_text(screen_width() * 0.5f32, 40f32, "Press Space to Resume", font);
+                write_text(screen_width() * 0.5f32, 75f32, "Press R to Reset", font);
             }
         }
         
